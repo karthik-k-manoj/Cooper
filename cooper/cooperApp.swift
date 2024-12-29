b752211972ff384c3673de95b441e05022d9881a
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-12-29 12:58:19 +0530

    add new project files

---
+++ cooper/cooperApp.swift
@@ -0,0 +1,17 @@
+//
+//  cooperApp.swift
+//  cooper
+//
+//  Created by Karthik K Manoj on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct cooperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
